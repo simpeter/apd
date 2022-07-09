f9e8941f9fd4fbb4e81ddd9ded068f12ca5952ac
Simon Peter <simon@cs.utexas.edu>
2022-07-08 17:46:06 -0700

    update.

--- start_vms.sh
+++ start_vms.sh
@@ -1,13 +1,64 @@
 #!/bin/bash -x
 
-nvms=19
+# Configuration
+n_machines=`geni-get manifest | xpath -e 'rspec/data_set/data_item[attribute::name = "emulab.net.parameter.n_machines"]/text()' -q`
+
+### Configuration
+# Number of VMs
+nvms=3
+
+function get_token()
+{
+    awk -F, "\$1 == \"machine$1\" { print \$2 }" /tmp/tokens.csv
+}
+
+# Generate cluster join tokens
+for i in `seq $((n_machines - 1))`; do
+    sudo lxc cluster add machine$i
+done
+sudo lxc cluster list-tokens -f csv > /tmp/tokens.csv
+
+# Join all cluster nodes
+for i in `seq $((n_machines - 1))`; do
+    ssh -oStrictHostKeyChecking=no machine$i "sudo lxd init --preseed <<EOF
+config: {}
+networks: []
+storage_pools: []
+profiles: []
+projects: []
+cluster:
+  server_name: machine$i
+  enabled: true
+  member_config:
+  - entity: storage-pool
+    name: local
+    key: size
+    value: 20GB
+    description: '\"size\" property for storage pool \"local\"'
+  - entity: storage-pool
+    name: local
+    key: source
+    value: \"\"
+    description: '\"source\" property for storage pool \"local\"'
+  - entity: storage-pool
+    name: local
+    key: zfs.pool_name
+    value: \"\"
+    description: '\"zfs.pool_name\" property for storage pool \"local\"'
+  cluster_address: machine0:8443
+  server_address: machine$i:8443
+  cluster_password: \"\"
+  cluster_certificate_path: \"\"
+  cluster_token: \"`get_token $i`\"
+EOF"
+done
 
 # Launch $nvms VMs on LXD cluster
 for i in `seq $nvms`; do sudo lxc launch images:ubuntu/20.04/cloud vm$i --vm -c limits.memory=4GB; done
 
 # Install docker on all VMs
 for i in `seq $nvms`; do
-    lxc exec vm$i -n -- sh -c "
+    sudo lxc exec vm$i -n -- sh -c "
 apt-get update
 apt-get install --yes curl
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -16,7 +67,7 @@ done
 wait
 
 # Install DeathStarBench
-lxc exec vm1 -- sh -c "
+sudo lxc exec vm1 -- sh -c "
 sudo apt-get install --yes git
 mkdir projects
 cd projects
--- virtualize.sh
+++ virtualize.sh
@@ -6,21 +6,13 @@
 set -x
 exec 1>/tmp/virtualize.log 2>&1
 
-# Configuration
-n_machines=3
-
-function get_token()
-{
-    awk -F, "\$1 == \"machine$1\" { print \$2 }" /tmp/tokens.csv
-}
-
 #echo "running test" > /tmp/test.txt
 #echo $PWD >> /tmp/test.txt
 #echo $USER >> /tmp/test.txt
 
 # With virtualization
 sudo apt-get update
-sudo apt-get install --yes snapd
+sudo apt-get install --yes snapd libxml-xpath-perl
 sudo snap install lxd
 
 # The rest runs only on machine0
@@ -69,44 +61,3 @@ cluster:
   cluster_certificate_path: ""
   cluster_token: ""
 EOF
-
-# Generate cluster join tokens
-for i in `seq $((n_machines - 1))`; do
-    sudo lxc cluster add machine$i
-done
-sudo lxc cluster list-tokens -f csv > /tmp/tokens.csv
-
-# Join all cluster nodes
-for i in `seq $((n_machines - 1))`; do
-    ssh -oStrictHostKeyChecking=no machine$i "sudo lxd init --preseed <<EOF
-config: {}
-networks: []
-storage_pools: []
-profiles: []
-projects: []
-cluster:
-  server_name: machine$i
-  enabled: true
-  member_config:
-  - entity: storage-pool
-    name: local
-    key: size
-    value: 20GB
-    description: '\"size\" property for storage pool \"local\"'
-  - entity: storage-pool
-    name: local
-    key: source
-    value: \"\"
-    description: '\"source\" property for storage pool \"local\"'
-  - entity: storage-pool
-    name: local
-    key: zfs.pool_name
-    value: \"\"
-    description: '\"zfs.pool_name\" property for storage pool \"local\"'
-  cluster_address: machine0:8443
-  server_address: machine$i:8443
-  cluster_password: \"\"
-  cluster_certificate_path: \"\"
-  cluster_token: \"`get_token $i`\"
-EOF"
-done
