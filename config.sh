c7fb15802743e1a440c8a7035d2792b57e3fe6dd
Simon Peter <simon@cs.utexas.edu>
2022-07-21 00:31:48 -0700

    update.

--- config.sh
+++ config.sh
@@ -6,5 +6,5 @@ n_servers=`geni-get manifest | xpath -e 'rspec/data_set/data_item[attribute::nam
 # Number of clients
 n_clients=`geni-get manifest | xpath -e 'rspec/data_set/data_item[attribute::name = "emulab.net.parameter.n_clients"]/text()' -q`
 
-# Number of VMs
-nvms=3
+# Number of VMs (same as servers)
+nvms=$n_servers
