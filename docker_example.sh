46f464568be5d884ae408d0363dca67ce42b8429
Simon Peter <simon@cs.utexas.edu>
2022-07-22 14:10:30 -0700

    update.

--- docker_example.sh
+++ docker_example.sh
@@ -26,6 +26,7 @@ sudo apt install python3-aiohttp libssl-dev libz-dev luarocks lua-socket
 sudo luarocks install luasocket
 make -C DeathStarBench/hotelReservation/wrk2
 ./wrk2/wrk -D exp -t 10 -c 100 -d 10 -L -s ./wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua http://server0:5000 -R 10000
+./wrk2/wrk -D exp -t 10 -c 100 -d 10 -L -s /local/repository/reserve_only.lua http://server0:5000 -R 1000
 
 # Test
 curl 'http://server0:5000/reservation?inDate=2015-04-19&outDate=2015-04-24&lat=nil&lon=nil&hotelId=9&customerName=Cornell_1&username=Cornell_1&password=1111111111&number=1'
