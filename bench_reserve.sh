7e768eac78602700fed6cfcef89bf955fe19b55d
Simon Peter <simon@cs.utexas.edu>
2022-07-22 20:43:21 -0700

    update.

---
+++ bench_reserve.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+time curl 'http://server0:5000/reservation?inDate=2015-04-10&outDate=2015-04-15&lat=nil&lon=nil&hotelId=9&customerName=Cornell_1&username=Cornell_1&password=1111111111&number=1'
