be647e697da5078c5a5a752f79b7204739be750f
simpeter <simpeter@localhost>
2022-08-06 00:12:06 -0600

    update.

--- kill_reservation.sh
+++ kill_reservation.sh
@@ -2,7 +2,9 @@
 
 HOST=`sudo docker service ps hotelreservation_mongodb-reservation | awk '$5 == "Running" { print $4 }'`
 
-ssh $HOST '
+ssh -oStrictHostKeyChecking=no $HOST '
 CID=`sudo docker container ls --filter name=hotelreservation_mongodb-reservation --format "{{.ID}}"`
-sudo docker inspect -f '{{.State.Pid}} {{.Name}}' $CID | awk '{ print $1 }'
+PID=`sudo docker inspect -f "{{.State.Pid}} {{.Name}}" $CID | cut -f1,1 -d" "`
+echo Killing $PID on `hostname`
+sudo kill -9 $PID
 '
