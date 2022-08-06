46f81e51f131e4f5320dc8f857c825f1d9b17b1c
Simon Peter <simpeter@cs.washington.edu>
2022-08-06 14:18:23 -0700

    update.

--- start_client.sh
+++ start_client.sh
@@ -4,6 +4,6 @@ export MAKEFLAGS=-j
 
 # Build wrk2
 cd /local/repository/DeathStarBench
-sudo apt-get install --yes python3-aiohttp libssl-dev libz-dev luarocks lua-socket
+sudo apt-get install --yes python3-aiohttp libssl-dev libz-dev luarocks lua-socket nfs-kernel-server
 sudo luarocks install luasocket
 make -C hotelReservation/wrk2
--- start_nfs.sh
+++ start_nfs.sh
@@ -1,5 +1,21 @@
 #!/bin/bash -x
 
+. config.sh
+
+# This script runs only on server0
+test x`hostname -s` == xserver0 || { echo "This script has to run on server0"; exit 1; }
+
+ssh client0 "
+sudo mkdir /mnt/reservation
+sudo bash 'echo -e \'/mnt/reservation\t*(rw,no_root_squash)\' >> /etc/exports'
+sudo /etc/init.d/nfs-kernel-server reload"
+
+for i in `seq 0 $((n_servers - 1))`; do
+    ssh -oStrictHostKeyChecking=no server$i "
+sudo mkdir -p /mnt/reservation
+sudo mount -t nfs client0:/mnt/reservation /mnt/reservation"
+done
+
 sudo docker service create --name registry --publish published=4000,target=5000 registry:2
 sudo docker compose -f hotelreservation-nfs.yml build
 sudo docker compose -f hotelreservation-nfs.yml push
