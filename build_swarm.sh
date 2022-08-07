ddebd61cf71ed94a1b1d8164af77fa8c9ae855d0
Simon Peter <simpeter@cs.washington.edu>
2022-08-07 14:40:25 -0700

    update.

---
+++ build_swarm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+
+sudo docker service create --name registry --publish published=4000,target=5000 registry:2
+sudo docker compose -f hotelreservation-nfs.yml build
+sudo docker compose -f hotelreservation-nfs.yml push
+#sudo docker stack deploy --compose-file hotelreservation-nfs.yml hotelreservation
--- start_nfs.sh
+++ start_nfs.sh
@@ -15,8 +15,3 @@ for i in `seq 0 $((n_servers - 1))`; do
 sudo mkdir -p /mnt/reservation
 sudo mount -t nfs client0:/mnt/reservation /mnt/reservation"
 done
-
-sudo docker service create --name registry --publish published=4000,target=5000 registry:2
-sudo docker compose -f hotelreservation-nfs.yml build
-sudo docker compose -f hotelreservation-nfs.yml push
-#sudo docker stack deploy --compose-file hotelreservation-nfs.yml hotelreservation
