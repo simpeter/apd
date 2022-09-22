a195ed33925d727b057e5654e52ff4fb58d6ac54
Simon Peter <simpeter@cs.washington.edu>
2022-09-22 15:58:08 -0700

    update.

--- profile.py
+++ profile.py
@@ -35,7 +35,7 @@ pc.defineParameter("n_clients", "Number of client machines",
                    portal.ParameterType.INTEGER, 1, advanced=True)
 
 pc.defineParameter("node_type", "Node Type (for lab1 only)",
-                   portal.ParameterType.NODETYPE, "c6525-25g", legalValues=["c6525-25g", "c6525-100g"], advanced=True)                   
+                   portal.ParameterType.NODETYPE, "c6525-25g", legalValues=["c6525-25g", "c6525-100g"])
 
 # Parameter to set virtualized mode or not
 modelist = [
